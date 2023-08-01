991fafeaacaaed03d05c76aa797f70e9616bdcdb
Néstor Patricio Rojas Ríos <np.rojas@gmail.com>
2023-07-31 21:54:40 -0400

    Sexta clase de Minería de datos

---
+++ Minería de datos/Clases/Clase 6/Ejemplo K Means.R
@@ -0,0 +1,181 @@
+# Instalamos los paquetes necesarios, en caso que no los tengamos instalados
+install.packages("cluster")
+install.packages("factoextra")
+install.packages("tidyverse")
+
+# Cargamos las librerias que utilizaremos
+library(cluster)
+library(factoextra)
+library(tidyverse)
+
+# Indicamos el directorio de trabajo
+setwd("C:/Users/Sebastian/Documents/R")
+
+# Cargamos la base de datos
+datos <- read.csv("faithful.csv", sep=";",header=TRUE)
+
+# Normalizamos los datos
+normalize <- function(x) { return ((x - min(x)) / (max(x) - min(x))) }
+
+datos_norm <- as.data.frame(lapply(datos, normalize))
+
+# K Means con K = 3
+k3 <- kmeans(datos_norm,
+             centers = 3,
+             nstart = 25)
+
+# Analicemos los resultados
+
+# Tamano de cada cluster
+k3$size
+# Cluster al que pertenece cada observacion
+k3$cluster
+# Centros de cada cluster
+k3$centers
+# Suma cuadratica inicial de distancias
+k3$totss
+# Sumas cuadraticas de distancias dentro de cada cluster
+k3$withinss
+k3$tot.withinss
+
+
+# Apliquemos el codo
+clusters <- c(1,2,3,4,5,6,7,8,9,10)
+codo <- c(0,0,0,0,0,0,0,0,0,0)
+for (k in 1:10) { 
+  codo[k] <- kmeans(datos_norm,
+                    centers = k,
+                    nstart = 25)$tot.withinss
+}
+
+plot(clusters, codo, type = "l")
+
+
+# Calculemos los valores de silueta para nuestros clusters
+silueta_k3 <- silhouette(k3$cluster, dist(datos_norm))
+summary(silueta_k3)
+
+# La primera columna es el cluster de cada observacion
+silueta_k3[,1]
+# La segunda columna es el cluster vecino de cada observacion
+silueta_k3[,2]
+# La tercera columna es el valor de silueta de cada observacion
+silueta_k3[,3]
+
+# Grafiquemos la silueta
+silueta <- c(0,0,0,0,0,0,0,0,0)
+for (k in 2:10) { 
+  modelo_aux <- kmeans(datos_norm,
+                       centers = k,
+                       nstart = 25)
+  silueta_aux <- silhouette(modelo_aux$cluster, dist(datos_norm))
+  silueta[k] <- mean(silueta_aux[, 3])
+}
+
+plot(clusters, silueta, type = "l")
+
+# Distribuciones de valores de silueta
+fviz_silhouette(silueta_k3)
+
+
+# Interpretemos que es cada cluster
+
+# Agreguemos el cluster de cada observacion a los datos
+datos$cluster3 <- k3$cluster
+
+# Podemos analizar visualmente los clusteres
+plot(datos$eruptions, datos$waiting, col = datos$cluster3)
+
+# Podemos analizar las variables promedio de cada cluster
+View(datos
+     %>% group_by(cluster3)
+     %>% summarise_all(mean))
+
+
+
+## Juguemos un poco con los datos de la evaluacion 3a ##
+
+datosT <- read.csv("SouthGermanCredit.csv", sep=";",header=TRUE)
+
+# En este ejemplo solo utilizaremos siete variables explicativas, por simplicidad
+# Usted deberá usar toda la información
+
+# Elegiremos como variables:
+# amount: variable continua
+# savings: variable ordinal numerica
+# age: variable continua
+# housing: variable categorica con 3 niveles
+# credits: variable ordinal numerica
+# persons: variable binaria
+# foreign: variable binaria
+
+datosT_small <- as.data.frame(cbind(amount = datosT$amount,
+                                    savings = datosT$savings,
+                                    age = datosT$age,
+                                    housing_free = (datosT$housing == 1),
+                                    housing_rent = (datosT$housing == 2),
+                                    credits = datosT$credits,
+                                    persons = datosT$persons,
+                                    foreign = datosT$foreign))
+
+
+# Normalizamos los datos
+normalize <- function(x) { return ((x - min(x)) / (max(x) - min(x))) }
+
+datosT_norm <- as.data.frame(lapply(datosT_small, normalize))
+
+# Considerando la cantidad de niveles en la variable categorica
+datosT_norm$housing_free <- datosT_norm$housing_free / 3
+datosT_norm$housing_rent <- datosT_norm$housing_rent / 3
+  
+# Codo
+clusters <- c(1,2,3,4,5,6,7,8,9,10)
+codo <- c(0,0,0,0,0,0,0,0,0,0)
+for (k in 1:10) { 
+  codo[k] <- kmeans(datosT_norm,
+                    centers = k,
+                    nstart = 25)$tot.withinss
+}
+
+plot(clusters, codo, type = "l")
+
+# Silueta
+silueta <- c(0,0,0,0,0,0,0,0,0,0)
+for (k in 2:10) { 
+  modelo_aux <- kmeans(datosT_norm,
+                       centers = k,
+                       nstart = 25)
+  silueta_aux <- silhouette(modelo_aux$cluster, dist(datosT_norm))
+  silueta[k] <- mean(silueta_aux[, 3])
+}
+
+plot(clusters, silueta, type = "l")
+
+# Usando K = 3
+modeloT3 <- kmeans(datosT_norm,
+                  centers = 3,
+                  nstart = 25)
+modeloT3$size
+
+
+# Usando K = 5
+modeloT5 <- kmeans(datosT_norm,
+                   centers = 5,
+                   nstart = 25)
+modeloT5$size
+
+# Agregamos los clusters a cada observacion
+datosT_small$cluster3 <- modeloT3$cluster
+datosT_small$cluster5 <- modeloT5$cluster
+
+
+# Valores promedios de variables en cada cluster con K = 3
+View(datosT_small
+     %>% group_by(cluster3)
+     %>% summarise_all(mean))
+
+
+# Valores promedios de variables en cada cluster con K = 5
+View(datosT_small
+     %>% group_by(cluster5)
+     %>% summarise_all(mean))
