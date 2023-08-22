ebbdc7739d8dbcac36fa487c7b25866cde24c936
Néstor Patricio Rojas Ríos <np.rojas@gmail.com>
2023-08-21 22:36:53 -0400

    Se agraga el material del curso de Machine learning

--- Minería de datos/Evaluaciones/Evaluación 3b/Tratamiento de la información.R
+++ Minería de datos/Evaluaciones/Evaluación 3b/Tratamiento de la información.R
@@ -49,6 +49,64 @@ playlists <- read.csv(
 # Desarrollo --------------------------------------------------------------
 
 # Pregunta 1 --------------------------------------------------------------
+# Se generan las transacciones
+transacciones <- playlists %>%
+  transactions(format = "long", cols = c("user", "artist"))
+summary(transacciones)
+
+# Se realiza una primera aproximación a los datos
+resultado_todos <- apriori(
+  data = transacciones,
+  parameter = list(
+    supp = 0.03,
+    conf = 0.05,
+    minlen = 2,
+    maxlen = 15,
+    maxtime = 15
+  )
+) %>%
+  sort(by = "support", decreasing = TRUE)
+summary(object = resultado_todos)
+inspect(x = resultado_todos)
+
+# El menor valor de confianza es de 0.029.
+# El mayor valor de confianza es de 1.
+# El menor valor de soporte es variable; con 0.0005 queda en 0.000533.
+# El mayor valor de soporte es de 0.0582.
 
 
+# Pregunta 3 --------------------------------------------------------------
+# Artistas a encontrar recomendaciones:
+# Coldplay, Jimi Hendrix, Metallica, Muse, Slayer, The Killers, The Shins, Wilco
 
+# Arctic Monkeys
+resultado_recomendacion_1 <- apriori(
+  data = transacciones,
+  parameter = list(
+    supp = 0.01,
+    conf = 0.04,
+    minlen = 2,
+    maxlen = 15,
+    maxtime = 15
+  ),
+  appearance = list(lhs = "arctic monkeys")
+) %>%
+  sort(by = "support", decreasing = TRUE)
+summary(object = resultado_recomendacion_1)
+inspect(x = resultado_recomendacion_1)
+
+# Beyonce
+resultado_recomendacion_2 <- apriori(
+  data = transacciones,
+  parameter = list(
+    supp = 0.01,
+    conf = 0.04,
+    minlen = 2,
+    maxlen = 15,
+    maxtime = 15
+  ),
+  appearance = list(lhs = "beyonce")
+) %>%
+  sort(by = "support", decreasing = TRUE)
+summary(object = resultado_recomendacion_2)
+inspect(x = resultado_recomendacion_2)
